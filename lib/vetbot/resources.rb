903998dc0f34393d8b80aa8629d1218a3d8acec5
John Mason <john@johnmason.io>
2016-12-28 17:02:29 -0500

    Start formatted resources

--- lib/vetbot/resources.rb
+++ lib/vetbot/resources.rb
@@ -2,6 +2,30 @@ require "yaml"
 
 module VetBot
   class << self
+    def formatted_resources(target)
+      hsh = resources(target)
+      return "No resources exist for #{target} yet" unless hsh
+
+      response = ""
+      hsh.keys.each do |section|
+        title = "Recommended #{section} are:"
+
+        arr = case section
+              when "Books"
+                books(hsh[section])
+              else
+                hsh[section].map do |items|
+                  items.map { |k, v| "#{k}: #{v}" }.join(", ")
+                end
+              end
+
+        arr.unshift(title)
+        response += arr.join("\n")
+      end
+
+      response
+    end
+
     def resources(target = nil)
       return unless target
 
@@ -10,6 +34,14 @@ module VetBot
       return unless File.exist? path
 
       YAML.load_file(path)
+    end
+
+    private
+
+    def books(array_of_hashes)
+      array_of_hashes.map do |hsh|
+        "#{hsh['title']} by #{hsh['author']} (#{hsh['link']})"
+      end
     end
   end
 end
--- spec/lib/vetbot/resources_spec.rb
+++ spec/lib/vetbot/resources_spec.rb
@@ -2,6 +2,7 @@ require "spec_helper"
 
 RSpec.describe "VetBot resources commands" do
   subject(:bot) { VetBot }
+  let(:ruby_resources) { File.join("resources/ruby.yml") }
 
   describe "#resources" do
     it "returns nil if not given an argument" do
@@ -9,8 +10,7 @@ RSpec.describe "VetBot resources commands" do
     end
 
     it "loads a hash from a YAML file based off argment" do
-      path = File.join("resources/ruby.yml")
-      expect(File.exist?(path)).to be true
+      expect(File.exist?(ruby_resources)).to be true
 
       resources = bot.resources("ruby")
       expect(resources).to_not be_nil
@@ -20,6 +20,38 @@ RSpec.describe "VetBot resources commands" do
     it "returns nil if a resource YAML file is not available" do
       expect(File).to receive(:exist?).and_return(false)
       expect(bot.resources("ruby")).to be_nil
+    end
+  end
+
+  describe "#formatted_resources" do
+    let(:resources) do
+      {
+        "Books" => Array.new(3) do |i|
+          {
+            "author" => "author#{i}",
+            "title"  => "title#{i}",
+            "link"   => "link#{i}"
+          }
+        end
+      }
+    end
+
+    it "returns a message if there are not any resources" do
+      expect(bot).to receive(:resources).and_return(nil)
+      response = bot.formatted_resources("foo")
+      expect(response).to eq("No resources exist for foo yet")
+    end
+
+    it "returns a single string separated by newlines" do
+      expected_str = \
+        "Recommended Books are:\n" \
+        "title0 by author0 (link0)\n" \
+        "title1 by author1 (link1)\n" \
+        "title2 by author2 (link2)"
+
+      expect(bot).to receive(:resources).and_return(resources)
+      response = bot.formatted_resources("foo")
+      expect(response).to eq(expected_str)
     end
   end
 end
